efd8d8e2f0cd20da68a46901d9446d2dd3270ce4
casey369 <casey369@morris.umn.edu>
2017-10-26 11:55:37 -0500

    Adds and begins EchoClient.java and EchoServer.java

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,31 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("127.0.0.1", 6013);
+			OutputStream output = socket.getOutputStream();
+			int toWrite;
+			
+			while((toWrite = System.in.read()) != -1){
+				System.out.println("enter something");
+				output.write(toWrite);
+			}
+			
+			int toPrint;
+			InputStream input = socket.getInputStream();
+			while((toPrint = input.read()) != -1){
+				System.out.println(toPrint);
+			}
+			
+			input.close();
+			output.close();
+			socket.close();
+		} catch (IOException ioe) {
+			System.out.println("We caught an exception");
+			System.err.println(ioe);
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,30 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(6013);
+
+			while (true) {
+				System.out.println("Got a request!");
+				Socket client = sock.accept();
+
+				InputStream input = client.getInputStream();
+				OutputStream output = client.getOutputStream();
+				int toWrite;
+				
+				while((toWrite = input.read()) != -1){
+					output.write(toWrite);
+				}
+				
+				input.close();
+				output.close();
+				client.close();
+			}
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 }
